f3537113e0be4f1bd111d678e78e4c75c996d2f2
jacob322 <jacob322@gmail.com>
2019-10-29 22:54:17 -0400

    Main menu functional & Death screen
    Main menu can access options, options works, death screen mockup

---
+++ Project Shadow/Assets/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ContinueGame()
+    {
+        int currentLevel = SceneManager.GetActiveScene().buildIndex;
+        if (currentLevel < 2)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else SceneManager.LoadScene(1);
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void FullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
