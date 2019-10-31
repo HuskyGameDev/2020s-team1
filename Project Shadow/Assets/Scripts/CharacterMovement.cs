6b4fb1a6d97f129cf1d03d61e306f0b8953458dd
Nam Nguyen <54714630+namnguyennhn@users.noreply.github.com>
2019-10-30 22:30:46 -0400

    Player's abilities
    Turn light source on and off
    Collect keys
    Collect fuel cells
    Keep track keys and light source

--- Project Shadow/Assets/Scripts/CharacterMovement.cs
+++ Project Shadow/Assets/Scripts/CharacterMovement.cs
@@ -1,14 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterMovement : MonoBehaviour
 {
     public float moveSpeed = 5;
     public Rigidbody2D rb;
     Vector2 movement;
-    public Transform transform;
-    // Start is called before the first frame update
+    public Transform characterTrnsform;
+    //public float fuelType1Amount;
+    public LightSourceControl lightSourceControl;
+    public Text txtKey;
+
+    //Start is called before the first frame update
     void Start()
     {
         
@@ -21,11 +27,11 @@ public class CharacterMovement : MonoBehaviour
         movement.x = hInput;
         if(hInput < 0)
         {
-            transform.eulerAngles = new Vector3(0, 0, -90);
+            characterTrnsform.eulerAngles = new Vector3(0, 0, -90);
         }
         else if(hInput > 0)
         {
-            transform.eulerAngles = new Vector3(0, 0, 90);
+            characterTrnsform.eulerAngles = new Vector3(0, 0, 90);
         }
 
 
@@ -34,11 +40,11 @@ public class CharacterMovement : MonoBehaviour
 
         if (vInput < 0)
         {
-            transform.eulerAngles = new Vector3(0, 0, 0);
+            characterTrnsform.eulerAngles = new Vector3(0, 0, 0);
         }
         else if (vInput > 0)
         {
-            transform.eulerAngles = new Vector3(0, 0, 180);
+            characterTrnsform.eulerAngles = new Vector3(0, 0, 180);
         }
 
     }
@@ -46,5 +52,22 @@ public class CharacterMovement : MonoBehaviour
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        FuelType1 fuelType1 = collision.GetComponent<FuelType1>();
+        if (fuelType1 != null)
+        {
+            lightSourceControl.fuelIncrease(fuelType1.fuelAmount);
+            fuelType1.destroy();
+        }
+
+        Key k = collision.GetComponent<Key>();
+        if (k != null)
+        {
+            txtKey.text = Convert.ToString(Convert.ToInt32(txtKey.text) + 1);
+            k.destroy();
+        }
     }
 }
---
+++ Project Shadow/Assets/Scripts/LightSourceControl.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightSourceControl : MonoBehaviour
+{
+    public Light lightSource;
+    private bool isLightOn = false;
+    public float fuelDecreasePerSecond;
+    public float currentFuelLevel;
+    public SpriteRenderer fuelBar;
+    private float maxFuelLevel;
+    public float fuelBarSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxFuelLevel = currentFuelLevel;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isLightOn && currentFuelLevel > 0)
+        {
+            lightSource.intensity = 3;
+            isLightOn = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.Space) && isLightOn)
+        {
+            lightSource.intensity = 0;
+            isLightOn = false;
+        }
+
+        if (isLightOn)
+        {
+            fuelDecrease();
+            if(currentFuelLevel < 0)
+            {
+                currentFuelLevel = 0;
+                fuelBar.size = new Vector2(fuelBarSize * (currentFuelLevel / maxFuelLevel), fuelBar.size.y);
+                lightSource.intensity = 0;
+                isLightOn = false;
+            }
+            else
+            {
+                fuelBar.size = new Vector2(fuelBarSize * (currentFuelLevel / maxFuelLevel), fuelBar.size.y);
+            }
+            
+        }
+        
+
+    }
+
+    public void fuelDecrease()
+    {
+        currentFuelLevel -= fuelDecreasePerSecond*Time.deltaTime;
+    }
+
+    public void fuelIncrease(float fuel)
+    {
+        if(currentFuelLevel < maxFuelLevel)
+        {
+            currentFuelLevel += fuel;
+            if(currentFuelLevel > maxFuelLevel)
+            {
+                currentFuelLevel = maxFuelLevel;
+            }
+
+            fuelBar.size = new Vector2(fuelBarSize * (currentFuelLevel / maxFuelLevel), fuelBar.size.y);
+
+        }
+        
+    }
+}
