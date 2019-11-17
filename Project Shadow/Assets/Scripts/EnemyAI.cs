f0a761efda93c9e716bd826d8cfc064b7d81fa44
jacob322 <jacob322@gmail.com>
2019-11-17 17:41:31 -0500

    enemy will now follow the player

--- Project Shadow/Assets/Scripts/EnemyAI.cs
+++ Project Shadow/Assets/Scripts/EnemyAI.cs
@@ -2,53 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
- public class EnemyTerritory : MonoBehaviour
- {
-         public BoxCollider territory;
-         GameObject player;
-         bool playerInTerritory;
  
-         public GameObject enemy;
-         BasicEnemy basicenemy;
- 
-         // Use this for initialization
-         void Start ()
-         {
-             player = GameObject.FindGameObjectWithTag ("Player");
-             basicenemy = enemy.GetComponent <BasicEnemy> ();
-             playerInTerritory = false;
-         }
-     
-         // Update is called once per frame
-         void Update ()
-         {
-             if (playerInTerritory == true)
-             {
-                 basicenemy.MoveToPlayer ();
-             }
- 
-             if (playerInTerritory == false)
-             {
-                 basicenemy.Rest ();
-             }
-         }
- 
-         void OnTriggerEnter (Collider other)
-         {
-             if (other.gameObject == player)
-             {
-                 playerInTerritory = true;
-             }
-         }
-     
-         void OnTriggerExit (Collider other)
-         {
-             if (other.gameObject == player) 
-             {
-                     playerInTerritory = false;
-             }
-         }
- }
  
 
 public class EnemyAI : MonoBehaviour
@@ -69,7 +23,7 @@ public class EnemyAI : MonoBehaviour
          // Update is called once per frame
          void Update ()
          {
-             
+        MoveToPlayer();
          }
  
          public void MoveToPlayer ()
