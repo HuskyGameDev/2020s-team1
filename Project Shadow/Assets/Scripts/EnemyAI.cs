845015565df6d558503379699c90a9046f0e6663
jacob322 <jacob322@gmail.com>
2020-02-15 13:48:26 -0500

    Revert "Revert "fixed my mistake with enemy""
    This reverts commit 464645c1d02fda83c6d921308c5fb7825e171472.

--- Project Shadow/Assets/Scripts/EnemyAI.cs
+++ Project Shadow/Assets/Scripts/EnemyAI.cs
@@ -54,7 +54,8 @@ public class EnemyAI : MonoBehaviour
 
         // this locks rotation on x and y becase 2d objects only need to rotate on z axis
         transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
-
+       
+        //below is my work in progress
         /*
         Vector3 direction = transform.position;
         var rotation = Quaternion.LookRotation(direction);
