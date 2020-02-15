dae9187b332aa0c9788bee2714ff01fc175f6e2a
Duong Nguyen <59811507+duong852@users.noreply.github.com>
2020-02-15 12:33:45 -0500

    Revert "working on enemey rotation"
    This reverts commit 9ee5a829346e5b351f0d980bc41beff923be67f4.

--- Project Shadow/Assets/Scripts/EnemyAI.cs
+++ Project Shadow/Assets/Scripts/EnemyAI.cs
@@ -8,10 +8,9 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-
+    
     public GameObject target;
-    Vector3 pos;
-    private NavMeshAgent agent; //this is the part of enemy that recognized the navmesh which is used for navigation
+    private NavMeshAgent agent;
     public float speed = 3f;
     public float attack1Range = 1f;
     public int attack1Damage = 1;
@@ -30,7 +29,6 @@ public class EnemyAI : MonoBehaviour
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        // below lock rotation so enemy doesnt rotate on3d axis
         agent.updateRotation = false;
         agent.updateUpAxis = false;
 
@@ -47,38 +45,13 @@ public class EnemyAI : MonoBehaviour
     void Update()
     {
         Chase();
-        // below three lines lock z position so it doesnt go below the map
-        pos = transform.position;
+        Vector3 pos = transform.position;
         pos.z = 1;
         transform.position = pos;
-
-        // this locks rotation on x and y becase 2d objects only need to rotate on z axis
-        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
-
-        /*
-        Vector3 direction = transform.position;
-        var rotation = Quaternion.LookRotation(direction);
-        if (direction == new Vector3(1, 0, 0)) //if direction of vector is positive in x direction
-        {
-            rotation *= Quaternion.Euler(0, 0, 90); // rotate charater 90 degrees on z axis
-        }
-        if (direction == new Vector3(-1, 0, 0)) //if direction of vector is negative in x direction
-        {
-            rotation *= Quaternion.Euler(0, 0, 270); // rotate charater 270 degrees on z axis
-        }
-        if (direction == new Vector3(0, 1, 0)) //if direction of vector is positive in y direction
-        {
-            rotation *= Quaternion.Euler(0, 0, 180); // rotate charater 180 degrees on z axis
-        }
-        if (direction == new Vector3(0, -1, 0)) // if direction of vector is negative in y direction
-        {
-            rotation *= Quaternion.Euler(0, 0, 0); // rotate charater 0 degrees on z axis
-        }
-        transform.eulerAngles = rotation.eulerAngles; */
-
         int rand = Random.Range(1, 5);
-        GameObject room = (GameObject)rooms[rand];
+        GameObject room = (GameObject) rooms[rand];
         //Wander(room);
+        
     }
 
     public void Wander(GameObject room)
@@ -90,20 +63,21 @@ public class EnemyAI : MonoBehaviour
     public void Chase()
     {
         /*
+        //rotate to look at player
+        transform.LookAt (target.position);
+        transform.Rotate (new Vector3 (0, -90,-90), Space.Self);
+
+        //move towards player
+        
         if (Vector3.Distance (transform.position, target.transform.position) > attack1Range) 
-        {transform.Translate (new Vector3 (0, speed * Time.deltaTime, 0));}
+        {
+                transform.Translate (new Vector3 (0, speed * Time.deltaTime, 0));
+        }
         */
-
-        //transform.LookAt(target.transform.position);
-        //transform.Rotate(new Vector3(0, -90, -90), Space.Self);
-        agent.SetDestination(target.transform.position); // uses navmesh to find how to get to target
-        DebugDrawPath(agent.path.corners); //draws path on view screen
-
-
-
+        agent.SetDestination(target.transform.position);
+        DebugDrawPath(agent.path.corners);
     }
 
-    //this method just shows lines for where enemy is going, not necessary for function of pathfinding
     public static void DebugDrawPath(Vector3[] corners)
     {
         if (corners.Length < 2) { return; }
